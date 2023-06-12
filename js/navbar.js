cd87303cdbd9b3dd11ea6a6cd4805f3de7ce6243
ThomasGG <thomasgg1203@gmail.com>
2023-06-11 21:38:59 -0500

    maquetado de todo el crud de productos

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 function Menu() {
-  let menu = document.querySelector('.menu');
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    let menu = document.querySelector('.menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
 
 
@@ -8,24 +8,34 @@ function Menu() {
 let slideIndex = 1;
 showSlides(slideIndex)
 
-function plusSlides(n){
+function plusSlides(n) {
     showSlides(slideIndex += n)
 }
-function currentSlide(n){
+function currentSlide(n) {
     showSlides(slideIndex = n)
 }
-function showSlides(n){
+function showSlides(n) {
     let i;
     let slides = document.querySelectorAll(".mySlides");
-    let quadrates = document.querySelectorAll(".quadrate"); 
-    if(n > slides.length) slideIndex = 1
-    if(n < 1) slideIndex = slides.length
-    for(i = 0; i < slides.length; i++){
+    let quadrates = document.querySelectorAll(".quadrate");
+    if (n > slides.length) slideIndex = 1
+    if (n < 1) slideIndex = slides.length
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"
     }
-    for(i = 0; i < quadrates.length;i++){
-        quadrates[i].className = quadrates[i].className.replace("active","")
+    for (i = 0; i < quadrates.length; i++) {
+        quadrates[i].className = quadrates[i].className.replace("active", "")
     }
-    slides[slideIndex-1].style.display = "block";
-    quadrates[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    quadrates[slideIndex - 1].className += " active";
+}
+//Ventaja emergente
+function showAlert() {
+    var alertBox = document.getElementById("alertBox");
+    alertBox.style.display = "block";
+}
+
+function closeAlert() {
+    var alertBox = document.getElementById("alertBox");
+    alertBox.style.display = "none";
 }
